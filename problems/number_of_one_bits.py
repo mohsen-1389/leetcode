46242342650ea85831a3e6e8bd948fde018ce057
mohsen <mohsen.computerscience@gmail.com>
2024-01-02 11:09:04 +0330

    number of one bits problem added to project.

---
+++ problems/number_of_one_bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        n = format(n, 'b')
+        ones = 0
+        for i in n:
+            if i == '1':
+                ones = ones+1
+            
+        return ones
+print(Solution().hammingWeight(678))
